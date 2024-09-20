65668b9d20763d8c14d0e75713e2d9b0fdaae479
krishkad <krishkadam88@gmail.com>
2024-09-20 12:47:36 +0530

    hero section done

--- src/components/shared/hero.tsx
+++ src/components/shared/hero.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 const Hero = () => {
     return (
-        <div className='w-full bg-green-50'>
+        <div className='w-full bg-slate-50'>
             <MaxWidthWrapper className="w-full lg:grid lg:grid-cols-3 py-12 lg:py-20 xl:py-24">
                 <section className="col-span-2 max-lg:pb-4">
                     <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
@@ -87,13 +87,62 @@ const Hero = () => {
                                     <Star className='w-4 h-4 shrink-0 text-green-600 fill-green-600' />
                                 </div>
                                 <p>
-                                    <span className='font-semibold'>1.250</span> happy customers
+                                    <span className='font-semibold'>1,250</span> happy customers
                                 </p>
                             </div>
                         </div>
                     </div>
                 </section>
-                <section className="col-span-full lg:col-span-1 w-full bg-green-300">ok</section>
+                <section className="col-span-full lg:col-span-1 w-full flex justify-center px-8 mt-8 h-fit">
+                    <div className='relative md:max-w-xl'>
+
+                        <Image
+                            width={0}
+                            height={0}
+                            quality={100}
+                            src="/your-image.png"
+                            alt="your image"
+                            unoptimized={true}
+                            className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden md:block lg:hidden xl:block"
+                        />
+
+                        <Image
+                            width={0}
+                            height={0}
+                            unoptimized={true}
+                            quality={100}
+                            src="/line.png"
+                            alt="line"
+                            className="absolute -left-6 -bottom-6 select-none w-20"
+                        />
+
+                        <div className="relative pointer-events-none z-50 overflow-hidden w-64">
+                            <Image
+                                width={0}
+                                height={0}
+                                src={'/phone-template-white-edges.png'}
+                                className='pointer-events-none z-50 select-none min-w-full min-h-full'
+                                quality={90}
+                                priority
+                                placeholder='empty'
+                                unoptimized={true}
+                                alt='image template'
+                            />
+                            <div className="absolute -z-10 inset-0">
+                                <Image
+                                    width={0}
+                                    height={0}
+                                    src={'/horse.jpg'}
+                                    className='object-cover min-w-full min-h-full'
+                                    quality={90}
+                                    priority
+                                    alt='hours'
+                                    unoptimized={true}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </MaxWidthWrapper>
         </div>
     )
