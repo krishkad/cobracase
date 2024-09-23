c7d164c16224ed85ad5abfd1a396bf8cb5061e0d
krishkad <krishkadam88@gmail.com>
2024-09-23 17:53:08 +0530

    reviews and underline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/shared/hero";
 import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
 import { Check, Star } from "lucide-react";
 import Image from "next/image";
+import Icons from "@/components/shared/icons";
 
 export default function Home() {
   return (
@@ -12,7 +13,12 @@ export default function Home() {
         <MaxWidthWrapper className="py-12 sm:py-20 flex flex-col  gap-16 sm:gap-32">
           <div className="mx-auto text-center">
             <h2 className="text-5xl md:text-6xl font-bold text-balance tracking-tight !leading-tight text-gray-900">
-              What our customers say
+              What our{" "}
+              <span className="relative px-2">
+                customers{" "}
+                <Icons.underline className="absolute -bottom-6 inset-x-0 text-green-600" />
+              </span>
+              say
             </h2>
           </div>
           <div className='mx-auto max-w-2xl lg:max-w-none px-4 space-y-10'>
@@ -28,10 +34,10 @@ export default function Home() {
               <div className="flex items-center gap-3">
                 <Image src={'/users/user-1.png'} width={40} height={40} className="rounded-full object-cover shrink-0" alt="user" />
                 <div className="">
-                  <p className="font-semibold">John</p>
+                  <p className="font-semibold">Chris T</p>
                   <p className="flex gap-1 items-center">
                     <Check className="w-4 h-4 stroke-2 text-green-600 shrink-0" />
-                    <span className="font-semibold text-gray-500 text-sm">Verified purchase</span>
+                    <span className="font-semibold text-gray-500 text-sm">Verified Purchase</span>
                   </p>
                 </div>
               </div>
@@ -51,7 +57,7 @@ export default function Home() {
                   <p className="font-semibold">Sarah W</p>
                   <p className="flex gap-1 items-center">
                     <Check className="w-4 h-4 stroke-2 text-green-600 shrink-0" />
-                    <span className="font-semibold text-gray-500 text-sm">Verified purchase</span>
+                    <span className="font-semibold text-gray-500 text-sm">Verified Purchase</span>
                   </p>
                 </div>
               </div>
@@ -64,14 +70,14 @@ export default function Home() {
                 <Star className="w-4 h-4 text-green-600 fill-green-600" />
                 <Star className="w-4 h-4 text-green-600 fill-green-600" />
               </div>
-              <p className="text-lg font-normal">&quot;This custom iPhone case is exactly what I was looking for! I wanted something personal, so I uploaded a photo of my family, and it turned out perfect. The picture is clear, and the colors are spot on. The case fits my phone perfectly and feels really durable. I love that its not just a pretty design but also provides solid protection. Its a great way to carry a special memory with me everywhere, and I am super happy with my purchase!&quot;</p>
+              <p className="text-lg font-normal">&quot;This custom iPhone case is exactly what I was looking for! I wanted something personal, so I uploaded a photo of my family, and it turned out perfect. The picture is clear, and the colors are spot on. The case fits my phone perfectly and feels really durable. I love that its not just a pretty design but also provides solid protection. Its a great way to carry a special memory with me everywhere, and I am super happy with my Purchase!&quot;</p>
               <div className="flex items-center gap-3">
                 <Image src={'/users/user-3.png'} width={40} height={40} className="rounded-full object-cover shrink-0" alt="user" />
                 <div className="">
                   <p className="font-semibold">Emily R</p>
                   <p className="flex gap-1 items-center">
                     <Check className="w-4 h-4 stroke-2 text-green-600 shrink-0" />
-                    <span className="font-semibold text-gray-500 text-sm">Verified purchase</span>
+                    <span className="font-semibold text-gray-500 text-sm">Verified Purchase</span>
                   </p>
                 </div>
               </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+
   	extend: {
   		colors: {
   			background: 'hsl(var(--background))',
