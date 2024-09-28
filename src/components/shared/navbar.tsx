c37f6e1a46476a6a850b5a4971449f25ef933819
krishkad <krishkadam88@gmail.com>
2024-09-28 17:31:23 +0530

    tring this method

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MaxWidthWrapper from './max-width-wrapper'
 import Image from 'next/image'
 import { Button } from '../ui/button'
@@ -11,6 +11,16 @@ import NavButtons from './nav-buttons'
 const Navbar = () => {
     const pathname = usePathname()
     const router = useRouter();
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        if (pathname.includes('auth')) {
+            setShow(true)
+        } else {
+            setShow(false);
+        }
+    }, [pathname])
+
     return (
         <div className='w-full z-50 sticky top-0 bg-slate-50/75 backdrop-blur-md'>
             <MaxWidthWrapper className='h-16 flex justify-between items-center'>
@@ -18,7 +28,7 @@ const Navbar = () => {
                     <Image src={'/snake-1.png'} width={0} height={0} className='w-9 h-9 object-contain' unoptimized={true} alt='cobra case logo' />
                 </Link>
 
-                {!pathname.includes('auth') ?
+                {!show ?
                     <NavButtons />
                     :
                     <Button variant={'outline'} onClick={() => router.push('/')} className={"flex items-center justify-center gap-2"} >
