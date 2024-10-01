05cbc17ecee1c1f8e3c5a2a125031f1d6b0b13e5
krishkad <krishkadam88@gmail.com>
2024-10-01 15:20:47 +0530

    try

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -32,26 +32,25 @@ function genId() {
   return count.toString()
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// type ActionType = typeof actionTypes
+type ActionType = typeof actionTypes
 
 type Action =
   | {
-    type:  typeof actionTypes["ADD_TOAST"]
-    toast: ToasterToast
-  }
+      type: ActionType["ADD_TOAST"]
+      toast: ToasterToast
+    }
   | {
-    type:  typeof actionTypes["UPDATE_TOAST"]
-    toast: Partial<ToasterToast>
-  }
+      type: ActionType["UPDATE_TOAST"]
+      toast: Partial<ToasterToast>
+    }
   | {
-    type:  typeof actionTypes["DISMISS_TOAST"]
-    toastId?: ToasterToast["id"]
-  }
+      type: ActionType["DISMISS_TOAST"]
+      toastId?: ToasterToast["id"]
+    }
   | {
-    type:  typeof actionTypes["REMOVE_TOAST"]
-    toastId?: ToasterToast["id"]
-  }
+      type: ActionType["REMOVE_TOAST"]
+      toastId?: ToasterToast["id"]
+    }
 
 interface State {
   toasts: ToasterToast[]
@@ -109,9 +108,9 @@ export const reducer = (state: State, action: Action): State => {
         toasts: state.toasts.map((t) =>
           t.id === toastId || toastId === undefined
             ? {
-              ...t,
-              open: false,
-            }
+                ...t,
+                open: false,
+              }
             : t
         ),
       }
